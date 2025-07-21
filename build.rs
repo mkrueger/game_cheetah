a5de579ebb9078f0451ae495851ae48c0166dbea
Mike Krüger <mkrueger@posteo.de>
2025-07-21 10:20:03 +0200

    Added optimizations.

--- build.rs
+++ build.rs
@@ -4,6 +4,13 @@ use {
 };
 
 fn main() -> io::Result<()> {
+    // Enable AVX2 if available
+    if std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64" {
+        println!("cargo:rustc-env=RUSTFLAGS=-C target-feature=+avx2");
+    }
+
+    // Compile Windows resources if on Windows
+    // This will embed an icon into the executable.
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
