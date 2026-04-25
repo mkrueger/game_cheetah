1f307f47e89df35d561f48c302c01672346a53cb
Mike Krüger <mkrueger@posteo.de>
2026-04-25 05:58:50 +0200

    Unify release artifact versioning

--- tools/prep_diz.py
+++ tools/prep_diz.py
@@ -1,26 +1,20 @@
-import sys
 import os
+import sys
+import tomllib
 
-if len(sys.argv) != 3:
-    print("need 2 arguments")
+if len(sys.argv) != 2:
+    print("need 1 argument")
     sys.exit(1)
 
-version=""
-cargo = open(os.path.join("Cargo.toml"), "r")
-for line in cargo.readlines():
-    if line.startswith("version"):
-        m = line.index('"')
-        version = line[m + 1:len(line) - 2]
-        break
-cargo.close()
+with open(os.path.join("Cargo.toml"), "rb") as cargo:
+    version = tomllib.load(cargo)["package"]["version"]
 
-file_id = open(os.path.join("build", "file_id.diz"), "r")
-lines = file_id.readlines()
-file_id.close()
-new_lines = list(map(lambda line: line.replace("#VERSION", version), lines))
+with open(os.path.join("build", "file_id.diz"), "r", encoding="utf-8") as file_id:
+    lines = file_id.readlines()
 
-f = open(sys.argv[2], "w")
-f.writelines(new_lines)
-f.close()
+new_lines = [line.replace("#VERSION", version) for line in lines]
+
+with open(sys.argv[1], "w", encoding="utf-8") as f:
+    f.writelines(new_lines)
 
 print(version)
